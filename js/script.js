e4fae46fd257d943743337685983ddb502c7c91a
Alexius Hale-Dubuque <alexiushaledubuque@yahoo.com>
2017-04-10 11:22:48 -0400

    Syntax modifications, error checking, and styles changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-//
+// Declare  the SoundCloud API Object as IIFE to use globally
 const apiObject = (() => {
-    return scApiObject = {
+    return ApiObject = {
         sc: 'http://api.soundcloud.com',
         clientId: '7d2a254767bd1fededc0ff2867c94419',
         search: '/search?q=',
@@ -19,7 +19,6 @@ const handleKeyPress = (e) => {
 const addEventListeners = () => {
     document.getElementById('submit_btn').addEventListener('click', getUserInput);
     document.getElementById('user_input').addEventListener('keypress', handleKeyPress);
-    // document.getElementById('hide').addEventListener('click', hide);
 }
 
 // Clear input fields and reset focus for next user input request
@@ -48,25 +47,23 @@ const getUserInput = () => {
     // Reset fields after input received
     clearFields(); 
 
-    // Format & get data from meetup api
     getSCApiData(userInput);
+    // gfApi(userInput);
 }
 
 // Create the url for JSONP
 const buildUrl = (url, endPoint, textInput, uID) => {
+
     if (endPoint === apiObject.search) {
         return `${url}${endPoint}${textInput}&client_id=${apiObject.clientId}`;
     } 
     
-    if (endPoint === apiObject.userTracks) {
-        endPoint = endPoint.replace(/\s/gi, uID);
-        return `${url}${endPoint}${textInput}&client_id=${apiObject.clientId}`;
-    }
+    endPoint === apiObject.userTracks ? 
+        url = `${url}${endPoint}${textInput}&client_id=${apiObject.clientId}` :
+        url = `${url}${endPoint}?client_id=${apiObject.clientId}`;
     
-    if (endPoint === apiObject.favorites) {
-        endPoint = endPoint.replace(/\s/gi, uID);
-        return `${url}${endPoint}?client_id=${apiObject.clientId}`;
-    }
+    url = url.replace(/\s/gi, uID);
+    return url;
 }
 
 // JSONP uses a script to fetch the data from api.soundcloud.com
@@ -116,13 +113,11 @@ const getSCApiData = (textInput) => {
 
 // Fetching more tracks from a specific user
 const getUserTracks = (userid) => {
+
     // Build URL
     let url = buildUrl(apiObject.sc, apiObject.userTracks, '', userid);
-    console.log("get user tracks url: ", url);
-    console.log('uid: ', userid);
 
     jsonp(url, (userTracks) => {
-        console.log(userTracks);
         userTracks.length > 0 ? (stopSpinner(), ulistTracks(userTracks)) : (document.getElementById('display').innerHTML = 
                 `<h4>No Groups Found!</h4>`, stopSpinner());
     });
@@ -130,14 +125,10 @@ const getUserTracks = (userid) => {
 
 // Fetching favoriters of a specific track
 const getFavoriters = (trackid) => {
-    // let url = 'http://api.soundcloud.com/tracks/35193992/favoriters?client_id=d6i0wruU7ddayTqrhwszluW0i9aNBlb1';
+   
     let url = buildUrl(apiObject.sc, apiObject.favorites, '', trackid);
 
-    // console.log('favoriters url: ', url);
-    // console.log('favoriters uri: ', uri);
-
     jsonp(url, (favoriters) => {
-        console.log(favoriters);
         favoriters.length > 0 ? (stopSpinner(), favoriteUsers(trackid, favoriters)) : (document.getElementById('display').innerHTML = 
                 `<h4>No Favoriters!</h4>`, stopSpinner());
     });
@@ -146,40 +137,40 @@ const getFavoriters = (trackid) => {
 // Sort favoriters according to  follower_count & last_name
 const sortByProp = (prop1, prop2) => {
    return function(a,b){
-      if( a[prop1] > b[prop1]){
+      if (a[prop1] > b[prop1]){
           return 1;
-      }else if( a[prop1] < b[prop1] ){
+      } else if( a[prop1] < b[prop1] ){
           return -1;
       }
       if( a[prop2] > b[prop2]){
           return 1;
-      }else if( a[prop2] < b[prop2] ){
+      } else if( a[prop2] < b[prop2] ){
           return -1;
       }
       return 0;
    }
 }
 
+// Render users whom favorited a track
 const favoriteUsers = (track, favList) => {
     let favoredTrack = '',
         string = '';
 
-    // console.log('Pre Sort List: \n', favList);
-    // Sorts the response data according to given properties
     favList.sort(sortByProp('followers_count', 'last_name'));
-    // console.log('Sorted List: \n', favList);
 
     document.getElementById('fav_users_list').innerHTML = '';
-    // console.log('User Track List: \n', JSON.stringify(userTrackList[0], null, 4));
 
     try {
             favoredTrack = `<div class=col_header><h2>${favList.length}&nbspUsers Favored Track:&nbsp${track}</h2></div><br>`;
             favList.forEach((fav) => {
-                // debugger; 
-                string += `<li><div class='f_user'><iframe src= ${fav.avatar_url} scrolling="no" 
-                align="middle" width="100" height="70" seamless></iframe><br><a href=${fav.permalink_url}>
-                <span id=fName>${fav.full_name}</span></a><br>${fav.followers_count}&nbsp Followers</div></li>`;  
-                // console.log('Track Details: \n', JSON.stringify(track.title, null, 4));
+                string += `<li><div><a href=${fav.permalink_url}>${fav.full_name}</a>`;
+
+                if (fav.artwork_url) {
+                    string += `<iframe src=${fav.artwork_url} scrolling="no" align="middle" 
+                    width="100" height="70" seamless></iframe><br>`    
+                }; 
+                
+                string += `</div></li>`;
             })
             document.getElementById('fav_users_list').innerHTML += `${favoredTrack}${string}`;  
     }   
@@ -193,26 +184,21 @@ const ulistTracks = (userTrackList) => {
     let user = '',
         string = '';
     document.getElementById('user_tracks_list').innerHTML = '';
-    // console.log('User Track List: \n', JSON.stringify(userTrackList[0], null, 4));
-    // artwork_url
 
     try {
         user = `<div class=col_header><h2>${userTrackList[0].user.username}'s Tracks</h2></div><br>`;
         userTrackList.forEach((item) => {
-            // debugger;
             string += `<li onclick='getFavoriters(${item.id})'>Track Title:&nbsp&nbsp ${item.title}<br>`;
             if (item.tag_list) {
                 string += `tags:&nbsp<em>${item.tag_list}</em><br></li><br>`;
             } 
 
-            string += `<iframe src= ${item.artwork_url} scrolling="no" 
-                align="middle" width="100" height="70" seamless></iframe></li><br>`;
-
-            // else {
-            //     string += `</li><br>`;
-            // }
-            // console.log('Track Details: \n', JSON.stringify(track.title, null, 4));
+            // Only render artwork if it exists
+            item.artwork_url ? 
+                string += `<iframe src=${item.artwork_url} scrolling="no" align="middle" 
+                width="100" height="70" seamless></iframe></li><br>` : string += `</li><br>`;
         })
+
         document.getElementById('user_tracks_list').innerHTML += `${user}${string}`;  
     }   
     catch(err) {
@@ -225,18 +211,18 @@ const listTracks = (tracks) => {
     let string = '';
     document.getElementById('tracklist').innerHTML = '';
 
-    // console.log(`length of data: ${tracks.collection.length}`);
-
     try {
         tracks.collection.forEach((track) => {
             if (track.user){
                 string += `<li onclick='getUserTracks(${track.user.id})'><span id='list_title'>Track Title:</span> ${track.title}<br>Username:&nbsp&nbsp ${track.user.username}<br>`;
                 if (track.tag_list) {
-                    string += `tags:&nbsp<em>${track.tag_list}</em><br></li><br>`;
-                } else {
-                    string += `</li><br>`;
-                }
-                // console.log('Track Details: \n', JSON.stringify(track.title, null, 4));
+                    string += `tags:&nbsp<em>${track.tag_list}</em><br>`;
+                } 
+
+                // Only render artwork if it exists
+                track.artwork_url ? 
+                    string += `<iframe src=${track.artwork_url} scrolling="no" align="middle" 
+                    width="100" height="70" seamless></iframe></li><br>` : string += `</li><br>`;
             }
         })
         show("#mood_tracks");
@@ -261,40 +247,14 @@ const css = (selector, property, value) => {
     }
 }
 
+// Show a dom element hidden in the css
 const show = (selector) => {
     css(selector, 'display', 'block');
 }
 
+// Hide a dom element 
 const hide = (selector) => {
     css(selector, 'display', 'none');
 }
 
 addEventListeners();
-
-// Giphy Api Request
-	// const gfApi = (() => {
-
- //  let api = {},
- //      baseUrl = 'http://api.giphy.com',
- //      path = '/v1/gifs/search',
- //      q = 'Religion+&+Spirituality'
- //      api_key = 'dc6zaTOxFJmzC';
-
- //  api.getData = function(callback) {
- //    requestUrl = baseUrl + path + '?q=' + q + '&api_key=' + api_key;
- //    request = new XMLHttpRequest();
- //    request.open('get', requestUrl, true);
- //    request.onload = function(e) {
- //      var response = request.response;
- //      response = JSON.parse(response);
- //      callback(response);
- //    };
- //    request.onerror = function(e) {
- //      callback(request.response, e);
- //    };
- //    request.send();
- //  };
-  
- //  return api;
-	
- // });
